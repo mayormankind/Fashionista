dd134ba9d89653d779aff1cd4dfe19709b93294e
Makinde Mayowa <mayowamakinde23@gmail.com>
2026-01-02 18:27:28 +0100

    Remove Header components from Home page
    Removed Header and HeaderMobile components from Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,10 @@ export default function Home() {
     <div className="flex w-full h-screen flex-col gap-5">
       <div className="relative hidden bg-cover w-full h-full md:flex" style={{backgroundImage:'url(assets/cover.png)'}}>
         <div className="flex flex-col w-full max-w-6xl h-full mx-auto">
-          <Header/>
           <HomeModal/>
         </div>
       </div>
       <div className="relative flex flex-col bg-cover w-full h-full md:hidden" style={{backgroundImage:'url(assets/i.webp)'}}>
-        <HeaderMobile/>
         <HomeModal/>
       </div>
     </div>
